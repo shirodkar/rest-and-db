ea5cab13bd0fdd917c02663a5c07cee11e5f0797
Sachin Shirodkar <sshirodk@redhat.com>
2025-10-03 22:25:56 +0000

    added uuid

--- src/main/java/org/acme/entity/AccessLog.java
+++ src/main/java/org/acme/entity/AccessLog.java
@@ -1,12 +1,15 @@
 package org.acme.entity;
 
 import java.util.Date;
+import java.util.UUID;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 
 @Entity
 public class AccessLog extends PanacheEntity {
+
+    public UUID id;
 
     public String requestValue;
 
